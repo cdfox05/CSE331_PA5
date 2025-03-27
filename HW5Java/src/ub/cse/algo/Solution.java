310c0d6739fefae3516cadc9e243a001f068fe62
cdfox05 <cdfox2@buffalo.edu>
2025-03-27 10:48:21 -0400

    finished dijkstra's implementation

--- HW5Java/src/ub/cse/algo/Solution.java
+++ HW5Java/src/ub/cse/algo/Solution.java
@@ -13,148 +13,114 @@ public class Solution {
         graph = g;
     }
 
-    class Node {
-        int node;
-        int weight;
-        ArrayList<Integer> edges;
-
-        public Node(int node, int weight)
-        {
-            this.node = node;
-            this.weight = weight;
-            edges = new ArrayList<>();
-        }
-    }
-
-    class Edge{
-        int to;
+    class PathEntry{ //makes keeping track of the path easier
         int from;
-
-        public Edge(int from, int to)
-        {
-            this.to = to;
-            this.from = from;
-        }
-    }
-
-    class PathEntry{
-        Edge edge;
+        int to;
         int weight;
 
-        public PathEntry(Edge e, int w)
+        public PathEntry(int e, int to, int w)
         {
-            this.edge = e;
+            this.from = e;
+            this.to = to;
             this.weight = w;
         }
     }
 
-    class Comparor implements Comparator<Node>
+    class Comparor implements Comparator<Integer> //compares the weights of each node for Priority Queue (Dijkstra's)
     {
         @Override
-        public int compare(Node a, Node b)
+        public int compare(Integer a, Integer b)
         {
-            if (a.weight > b.weight)
+            if (graph.get(a).get(0) > graph.get(b).get(0))
                 return 1;
 
             return -1;
         }
     }
     
-    public ArrayList<Integer> outputPath(){
+    public ArrayList<Integer> outputPath(){ //Dijkstra's via textbook documentations skeleton
         /*
          * Find the smallest weighted path between _startNode and _endNode
          * The first number of graph's adjacency list is the weight of it's node
          */
 
-        PriorityQueue<Node> todo = new PriorityQueue<>(new Comparor());
+        PriorityQueue<Integer> todo = new PriorityQueue<>(new Comparor());
 
-        HashMap<Integer, Node> nodeList = new HashMap<>();
-        HashMap<Integer, List<Edge>> g = new HashMap<>();
-        HashSet<Edge> edgesVisited = new HashSet<>();
-        HashSet<Node> nodesVisited = new HashSet<>();
-        HashMap<Integer, PathEntry> paths = new HashMap<>();
-        Node startNode = null;
-        Node endNode = null;
+        //HashMap<Integer, Integer> nodeList = new HashMap<>();
+        HashSet<Integer> edgesVisited = new HashSet<>(); //keep track of "edges" pretty much keeps track of nodes
+        HashMap<Integer, PathEntry> paths = new HashMap<>(); //path entry log
 
-        for (int key : graph.keySet())
+        graph.get(_startNode).set(0, 0); //sets start node weight to 0
+
+        todo.add(_startNode);
+
+        while (!todo.isEmpty()) //full Dijkstra's implementation
         {
-            ArrayList<Integer> adj = graph.get(key);
-            Node node = new Node(key, adj.remove(0));
+            int curr = todo.poll(); //grabs current node
 
-            if (key == _startNode)
-            {
-                node.weight = 0;
-                startNode = node;
-            }
-            else if (key == _endNode)
-                endNode = node;
-
-            node.edges = adj;
-            nodeList.put(key, node);
-
-            g.put(node.node, new ArrayList<>());
-            for (int to : adj)
-            {
-                g.get(node.node).add(new Edge(node.node, to));
-            }
-        }
-
-        todo.add(startNode);
-
-        while (!todo.isEmpty())
-        {
-            Node curr = todo.poll();
-            nodesVisited.add(curr);
-
-            if (curr == endNode)
+            if (curr == _endNode) { //breaks when target node is found
                 break;
-            if (!g.containsKey(curr.node))
+            }
+            if (!graph.containsKey(curr)) //skips nodes that aren't in the graph (null pointer)
             {
-                System.out.println(curr.node);
                 continue;
             }
 
-            for (Edge e : g.get(curr.node))
-            {
-                if (!edgesVisited.contains(e)) {
-                    Node node = nodeList.get(e.to);
+            int x = 0; //used to skip the weight in the current nodes edge list
 
-                    if (!paths.containsKey(node.node))
-                        paths.put(node.node, new PathEntry(e, curr.weight + node.weight));
+            for (int node : graph.get(curr)) //cycles through the current nodes edge list
+            {
+                if (x == 0)
+                {
+                    x++;
+                    continue;
+                }
+                if (!edgesVisited.contains(node)) {
+                    int nWeight = graph.get(node).get(0);
+                    int cWeight = graph.get(curr).get(0);
+
+                    if (!paths.containsKey(node)) { //if the paths doesn't already have the current edge/neighbor add it
+                        paths.put(node, new PathEntry(curr, node, cWeight + nWeight));
+                    }
                     else
                     {
-                        if (paths.get(node.node).weight > (curr.weight + node.weight))
-                            paths.put(node.node, new PathEntry(e, curr.weight + node.weight));
+                        if (paths.get(node).weight > (cWeight + nWeight)) //if new weight is smaller than old weight update the path
+                            paths.put(node, new PathEntry(curr, node,cWeight + nWeight));
                     }
 
-                    edgesVisited.add(e);
-                    node.weight+=curr.weight;
+                    graph.get(node).set(0,cWeight + nWeight); //updates weights for the comparator
+                    edgesVisited.add(node);
                     todo.add(node);
                 }
             }
 
         }
 
+        //System.out.println(paths);
         ArrayList<Integer> finalPath = new ArrayList<>();
-        Stack<Edge> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+        //System.out.println("START NODE: " + _startNode);
 
         int curr = _endNode;
-        while (curr != _startNode)
+        //System.out.println("CURR BEFORE STACK " + curr);
+        while (curr != _startNode) //Loops through until path followed back from end node gives us the starting node
         {
+            //System.out.println(curr);
             if (!paths.containsKey(curr))
                 return new ArrayList<Integer>();
             PathEntry pathEntry = paths.get(curr);
-            if (!stack.contains(pathEntry.edge));
+            if (!stack.contains(pathEntry.from));
             {
-                stack.push(pathEntry.edge);
+                stack.push(pathEntry.from);
             }
-            curr = pathEntry.edge.from;
+            curr = pathEntry.from;
+            //System.out.println("NEXT: "+ curr);
         }
 
-        while (!stack.isEmpty())
-            finalPath.add(stack.pop().from);
+        while (!stack.isEmpty()) //empties the stack into our path
+            finalPath.add(stack.pop());
 
-        finalPath.add(_endNode);
+        finalPath.add(_endNode); //adds end node since it is skipped in the while loop adding to stack
 
 
         return finalPath;
